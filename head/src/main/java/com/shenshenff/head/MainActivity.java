551efe25556991c1ca7b3709ee5a2ce98aa508e6
shenshengit <shenshen@shenshen.local>
2016-05-12 01:31:34 +0800

    01
    宫格滑动

---
+++ head/src/main/java/com/shenshenff/head/MainActivity.java
@@ -0,0 +1,98 @@
+package com.shenshenff.head;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.widget.LinearLayout;
+
+public class MainActivity extends FragmentActivity
+{
+    private String[] mTitles = new String[] { "" };
+    private LinearLayout mIndicator;
+    private ViewPager mViewPager;
+    private FragmentPagerAdapter mAdapter;
+    private TabFragment[] mFragments = new TabFragment[mTitles.length];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+        initDatas();
+        initEvents();
+    }
+
+    private void initEvents()
+    {
+        mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener()
+        {
+            @Override
+            public void onPageSelected(int position)
+            {
+            }
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset,
+                                       int positionOffsetPixels)
+            {
+                //mIndicator.scroll(position, positionOffset);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state)
+            {
+
+            }
+        });
+
+    }
+
+    private void initDatas()
+    {
+        //mIndicator.setTitles(mTitles);
+
+        for (int i = 0; i < mTitles.length; i++)
+        {
+            mFragments[i] = (TabFragment) TabFragment.newInstance(mTitles[i]);
+        }
+
+        mAdapter = new FragmentPagerAdapter(getSupportFragmentManager())
+        {
+            @Override
+            public int getCount()
+            {
+                return mTitles.length;
+            }
+
+            @Override
+            public Fragment getItem(int position)
+            {
+                return mFragments[position];
+            }
+
+        };
+
+        mViewPager.setAdapter(mAdapter);
+        mViewPager.setCurrentItem(0);
+    }
+
+    private void initViews()
+    {
+        mIndicator = (LinearLayout) findViewById(R.id.id_stickynavlayout_indicator);
+        mViewPager = (ViewPager) findViewById(R.id.id_stickynavlayout_viewpager);
+
+		/*
+		RelativeLayout ll = (RelativeLayout) findViewById(R.id.id_stickynavlayout_topview);
+		TextView tv = new TextView(this);
+		tv.setText("我的动态添加的");
+		tv.setBackgroundColor(0x77ff0000);
+		ll.addView(tv, new RelativeLayout.LayoutParams(
+				RelativeLayout.LayoutParams.MATCH_PARENT, 600));
+		*/
+    }
+
+}
